9693d2ac4520709f710be46df2fcb425bb7f9f8e
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-04-06 21:56:31 +0200

    func switch if else

--- script/main.js
+++ script/main.js
@@ -1,19 +1,43 @@
-let person = prompt("Хто ти?");
+// let person = prompt("Хто ти?");
 
 // if (person == "admin") {
 //     console.log('admin');
 // } else if (person == "user") {
 //     console.log('user');
 // }
+// -----------------------------
+// switch(person){
+//     case "admin":
+//         console.log('admin');
+//         break;
+//     case "user":
+//         console.log('user');
+//         break;
+//     default:
+//         console.log('?????');
+//         break
+// }
+// ------------------------------
+// const max = (a, b) => (a > b ? a : b);
 
-switch(person){
-    case "admin":
-        console.log('admin');
-        break;
-    case "user":
-        console.log('user');
-        break;
-    default:
-        console.log('?????');
-        break
-}
+// console.log( max(10, 8) );
+
+// // -----------------
+// const print = function(...args){
+//     console.log(...args);
+// }
+// print(1212, "wdwdw", 232, "dty", "1s1s3x3c", 5657);
+
+// ----------------------
+// let Com_car = ["Audi", "Ford", "BMW", " Skoda", "Ferrari"];
+// let Com_owner = ["Joe", "Martin", "Alex", "Max", "Hue"];
+
+// const print = (id, car, owner) => {
+//     return `${car[id]} ${owner[id]}`;
+// }
+
+// console.log(print(2, Com_car, Com_owner));
+// console.log(print(1, Com_car, Com_owner));
+// console.log(print(3, Com_car, Com_owner));
+// console.log(print(0, Com_car, Com_owner));
+// -------------------------
