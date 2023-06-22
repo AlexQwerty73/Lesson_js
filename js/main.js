da1a8d27459bfb33bac108e3ff33cc3929dc07b0
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-06-22 17:32:40 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const doc = document;
+const todos = [
+    'купити хліб',
+    'зробити дз',
+    'відпочити',
+    '????'
+];
+
+getTodosData=(todosArr)=>{
+    const todosArrObj = [];
+
+    for (let item of todosArr) {
+        todosArrObj.push(
+            {
+                id: todosArr.indexOf(item) + 1,
+                body: todosArr[todosArr.indexOf(item)],
+                complited: false
+            }
+        );
+    }
+
+    return todosArrObj;
+}
+
+console.log(getTodosData(todos));
